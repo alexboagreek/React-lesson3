4b024f59d6e21479dbffcdd1507cb3e8cc592d1c
Alex <olivesargreek@gmail.com>
2022-07-17 12:50:51 +0300

    feat: repeat lesson3 basic

--- lesson3/src/components/ClassComponent/ClassComponent.jsx
+++ lesson3/src/components/ClassComponent/ClassComponent.jsx
@@ -1,33 +1,69 @@
 import React from 'react';
 import style from './ClassComponent.module.css';
+import PropTypes from 'prop-types';
 
 export class ClassComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      number: 5,
+      result: 'Pезультат',
+      userNumber: '',
+      randomNumber:
+        Math.floor(Math.random() * this.props.max - this.props.min) +
+        this.props.min,
+      count: 0,
     };
   }
 
   handleSubmit = (event) => {
     event.preventDefault();
-    console.log(this.state.number);
+    this.setState(state => ({
+      count: this.state.count + 1,
+    }));
+    this.setState(state => {
+      if (!state.userNumber) {
+        return {
+          result: `Введите число`,
+        };
+      }
+
+      if (state.userNumber > state.randomNumber) {
+        return {
+          result: `${state.userNumber} больше загадонного числа`,
+        };
+      }
+
+      if (state.userNumber < state.randomNumber) {
+        return {
+          result: `${state.userNumber} меньше загадонного числа`,
+        };
+      }
+
+      return {
+        result: `Вы нострадамус - угадали загаданое число  ${state.userNumber},
+        потыток ${state.count}`,
+      };
+    });
   };
 
-  handleInput = (event) => {
-    console.log(event.target.value);
+  handleChange = (event) => {
+    this.setState({
+      userNumber: event.target.value,
+    });
   };
 
   render() {
     return (
       <div className={style.game}>
-        <p className={style.result}>{this.state.number}</p>
+        <p className={style.result}>{this.state.result}</p>
+
         <form className={style.form} onSubmit={this.handleSubmit}>
           <label className={style.label} htmlFor='user_number'>
             Угадай число
           </label>
+
           <input className={style.input} type='number' id='user_number'
-            onChange={this.handleInput}
+            onChange={this.handleChange} value={this.state.userNumber}
           />
           <button className={style.btn}>Угадать</button>
         </form>
@@ -35,3 +71,8 @@ export class ClassComponent extends React.Component {
     );
   }
 }
+
+ClassComponent.propTypes = {
+  min: PropTypes.number,
+  max: PropTypes.number,
+};
